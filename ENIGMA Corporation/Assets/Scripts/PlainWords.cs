a3a8748f0aa439987106fcf2d89a2a463cb85178
Vexentrios <118862342+Vexentrios@users.noreply.github.com>
2023-11-24 20:38:45 +0100

    Big Update for Level1
    Added:
    - Functionality for "Fence Breaker App"
    - Verification if user answer is correct
    - Working Progress Bar
    - Option to return to All Users Scene

---
+++ ENIGMA Corporation/Assets/Scripts/PlainWords.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlainWords : MonoBehaviour
+{
+    public static string[] FenceWords = {"corporation","university", "security",
+                                         "technophobia", "scopophobia", "administration",
+                                         "disagreement", "acknowledgements", "disqualification",
+                                         "ciphers", "technology"};
+}
