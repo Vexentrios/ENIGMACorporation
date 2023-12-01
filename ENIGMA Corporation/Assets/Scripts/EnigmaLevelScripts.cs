294ede41e63bacaafd862eaffe21c4d6b0a44425
Vexentrios <118862342+Vexentrios@users.noreply.github.com>
2023-12-01 13:30:25 +0100

    Working Enigma Script
    Added (after a lot of hours of thinking and understanding this algorithm), working ENIGMA for static Turnover type B and alphabetical Entry wheel type

--- ENIGMA Corporation/Assets/Scripts/EnigmaLevelScripts.cs
+++ ENIGMA Corporation/Assets/Scripts/EnigmaLevelScripts.cs
@@ -19,8 +19,6 @@ public class EnigmaLevelScripts : MonoBehaviour
     [SerializeField] private Button OpenRotors;
     [SerializeField] private Button CloseRotors;
     [SerializeField] private Button[] RotorsChoiceButtons = new Button[18];
-    [SerializeField] private Button[] RotorChangePosition = new Button[6];  //[0-1] Left | [2-3] Middle | [4-5] Right
-    [SerializeField] private Button[] Keyboard = new Button[26];
     [SerializeField] private Button SendAnswer;
     [SerializeField] private Button SwapPanels;
     [Header("-------------------------------------------------")]
@@ -39,11 +37,16 @@ public class EnigmaLevelScripts : MonoBehaviour
     [Header("Other")]
     [SerializeField] private Image[] ActiveRotors = new Image[3];
 
-    List <string> switchBoard = new List<string>();
-    bool switchBoardActive;
+    int[] index = new int[2];
+    List<char> switchBoard = new List<char> {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+    
     int[] switcherLetterIndex = new int[2];
 
-    List<string> enterRoller = new List<string> { "J", "W", "U", "L", "C", "M", "N", "O", "H", "P", "Q", "Z", "Y", "X", "I", "R", "A", "D", "K", "E", "G", "V", "B", "T", "S", "F" };
+    List<char> revertRoller = new List<char> { 'Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T' };
+
+    string[] RotorsAlphabets = { "EKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOE", "BDFHJLCPRTXVZNYEIWGAKMUSQO", "ESOVPZJAYQUIRHXLNFTGKDCMWB", "VZBRGITYUPSDNHLXAWMJQOFECK" };
+
+    int[] rotateRotor = { 17, 5, 22, 10, 0 };
 
     string alphabetDefault = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string alphabetRemains;
@@ -51,8 +54,6 @@ public class EnigmaLevelScripts : MonoBehaviour
     int[] chosenRotor = new int[3];
     int[] currentRotorPosition = new int[3];
     bool locked;
-    public static int keyColorID;
-    public static int usedXORKey;
     int hideTimer;
 
     void Start()
@@ -92,7 +93,7 @@ public class EnigmaLevelScripts : MonoBehaviour
         int rotorPosition = pressed % 3;
         if(chosenRotor[rotorPosition] != -1)
         {
-            int swapLine = chosenRotor[rotorPosition];
+            int swapLine = chosenRotor[rotorPosition] + 1;
             for (int o = 0; o < 3; o++)
             {
                 RotorsChoiceButtons[swapLine * 3 + o].gameObject.SetActive(true);
@@ -105,14 +106,14 @@ public class EnigmaLevelScripts : MonoBehaviour
         RotorsChoiceButtons[rotorPosition].interactable = true;
         RotorsChoiceButtons[pressed].interactable = false;
         RotorsChoiceButtons[pressed].gameObject.SetActive(true);
-        chosenRotor[rotorPosition] = rotorLine;
+        chosenRotor[rotorPosition] = rotorLine - 1;
 
         CheckKeyboardBlockade();
     }
 
     public void RemoveRotor(int pressed)
     {
-        int rotorLine = chosenRotor[pressed];
+        int rotorLine = chosenRotor[pressed] + 1;
         RotorsChoiceButtons[pressed].interactable = false;
         chosenRotor[pressed] = -1;
         for (int o = 0; o < 3; o++)
@@ -179,15 +180,17 @@ public class EnigmaLevelScripts : MonoBehaviour
 
     public void CreateEnigmaSwitch()
     {
-        if(!switchBoardActive)
-            switchBoardActive = true;
-
         SwitchBoard.text += alphabetRemains[switcherLetterIndex[0]].ToString() + " <-> " + alphabetRemains[switcherLetterIndex[1]].ToString() + "\n";
 
         char left = (char)(alphabetRemains[switcherLetterIndex[0]]);
         char right = (char)(alphabetRemains[switcherLetterIndex[1]]);
         alphabetRemains = alphabetRemains.Replace(left, '_').Replace(right, '_');
         alphabetRemains = alphabetRemains.Replace("_", "");
+
+        index[0] = switchBoard.IndexOf(left);
+        index[1] = switchBoard.IndexOf(right);
+        switchBoard[index[0]] = right;
+        switchBoard[index[1]] = left;
 
         if (alphabetRemains.Length > 0)
         {
@@ -204,15 +207,12 @@ public class EnigmaLevelScripts : MonoBehaviour
                 x.interactable = false;
             MakeSwitch.interactable = false;
         }
-
     }
 
     public void ClearSwitchboard()
     {
         SwitchBoard.text = "";
-        switchBoardActive = false;
-        switchBoard.Clear();
-        switchBoard.AddRange(enterRoller);
+        switchBoard.Sort();
         alphabetRemains = alphabetDefault;
         switcherLetterIndex[0] = 0;
         SwitchedLetters[0].text = alphabetRemains[switcherLetterIndex[0]].ToString();
@@ -221,7 +221,6 @@ public class EnigmaLevelScripts : MonoBehaviour
         foreach (Button x in SwitchLetter)
             x.interactable = true;
         MakeSwitch.interactable = true;
-
     }
 
     //########################################################################################
@@ -248,262 +247,71 @@ public class EnigmaLevelScripts : MonoBehaviour
     //###########################[Vigenere Cipher Part]#######################################
     //########################################################################################
 
-    /*public void OpenVigenereApp()
+    public void EnigmaMachine(string pressedKey)
     {
-        ActiveTool = EncryptTools.Vigenere;
-        VigenereKey.text = "";
-        VigenereAppIcon.GetComponent<Button>().enabled = false;
-        ResetVigenere();
-        VigenereApp.SetActive(true);
-    }
-
-    public void CloseVigenereApp()
-    {
-        VigenereApp.SetActive(false);
-        VigenereAppIcon.GetComponent<Button>().enabled = true;
-    }
-
-    public void GenerateVigenere()
-    {
-        string vinKey = VigenereKey.text.ToUpper();
-        ResetVigenere();
-        if (vinKey.Length > 0)
-            if (TestEnteredKey(vinKey))
-            {
-                int index = 0;
-                int alphaIndex;
-
-                while (index < vinKey.Length)
-                {
-                    alphaIndex = vinKey[index] - 65;
-                    VigenereKeyLetter[index].text = "Letter " + vinKey[index].ToString();
-                    for (int l = 0; l < 26; l++)
-                    {
-                        VigenereLine[index].text += alphabet[alphaIndex] + " ";
-                        alphaIndex = ((alphaIndex + 1) == 26 ? 0 : (alphaIndex + 1));
-                    }
-                    index++;
-                }
-            }
-            else
-            {
-                VigenereKey.text = "";
-                VigenereError.gameObject.SetActive(true);
-                hideTimer = 2;
-                StartCoroutine(ShowError());
-            }
-    }
-
-    public void ResetVigenere()
-    {
-        for (int v = 0; v < 10; v++)
+        currentRotorPosition[2] = (currentRotorPosition[2] + 1) > 25 ? 0 : currentRotorPosition[2] + 1;
+        if (currentRotorPosition[2] == rotateRotor[chosenRotor[2]])
         {
-            VigenereKeyLetter[v].text = "Letter -";
-            VigenereLine[v].text = "";
+            currentRotorPosition[1] = (currentRotorPosition[1] + 1) > 25 ? 0 : currentRotorPosition[1] + 1;
+            if (currentRotorPosition[1] == rotateRotor[chosenRotor[1]])
+            {
+                currentRotorPosition[0]++;
+                RotorPosition[0].text = (currentRotorPosition[0] + 1).ToString();
+            }
+            RotorPosition[1].text = (currentRotorPosition[1] + 1).ToString();
         }
+        RotorPosition[2].text = (currentRotorPosition[2] + 1).ToString();
+
+        char letter = pressedKey[0];
+        int posIndexer;
+        char result = (char)(switchBoard.IndexOf(letter) + 65);
+        Debug.Log(result);
+
+        for (int ent = 2; ent >= 0; ent--)
+        {
+            //Enter Rotor from Right to Left
+            result = RotorsAlphabets[chosenRotor[ent]][(result - 65 + currentRotorPosition[ent]) % 26];
+            Debug.Log(("Right: ", result));
+            //Leave Rotor from Right to Left
+            posIndexer = (result - 65 - currentRotorPosition[ent]) % 26;
+            if (posIndexer < 0)
+                posIndexer = 26 + posIndexer;
+            result = alphabetDefault[posIndexer];
+            Debug.Log(("Left: ", result));
+        }
+
+        //(Reflector) Revert Enter and Leave
+        posIndexer = (revertRoller[result - 65] - 65);
+        //result = revertRoller[result - 65];
+        Debug.Log(("Revert: ", posIndexer));
+
+        for (int leav = 0; leav < 3; leav++)
+        {
+            //Enter Rotor from Left to Right
+            result = alphabetDefault[(posIndexer + currentRotorPosition[leav]) % 26];
+            Debug.Log(("Leave: ", result));
+            //Leave Rotor from Left to Right
+            posIndexer = (RotorsAlphabets[chosenRotor[leav]].IndexOf(result) - currentRotorPosition[leav]) % 26;
+            if (posIndexer < 0)
+                posIndexer = 26 + posIndexer;
+            Debug.Log(("Right: ", posIndexer));
+        }
+
+        result = switchBoard[posIndexer];
+        Debug.Log(("Cabel Back:", result));
+        Debug.Log(result);
+        EnigmaScreen.text += result.ToString();
     }
-
-    //########################################################################################
-    //##############################[ENCRYPTION Part]#########################################
-    //########################################################################################
-
-    public void ADFVGX_PlayFair_Vigenere_EncryptFunction()
-    {
-        ///THIS CONTENT IS MOVED TO FINAL VERSION
-        //if (PlainWords.ADFVGX_PlayFair_Vigenere_Words.Count > 0)
-        if (PlainWords.ADFVGX_PlayFair_Vigenere_Words.Count > 0 && AccessEnigmaScript.decryptedMessagesLevelFour != goal)
-        {
-            int number = Random.Range(0, PlainWords.ADFVGX_PlayFair_Vigenere_Words.Count);
-            Level4Answer = PlainWords.ADFVGX_PlayFair_Vigenere_Words[number];
-            PlainWords.ADFVGX_PlayFair_Vigenere_Words.RemoveAt(number);
-            Level4EncryptedWord = "";
-            chosenKey = Random.Range(0, 15);
-            keyColorID = chosenKey / 3;
-
-            if (PlainWords.ADFVGXCodes > 0 && PlainWords.PlayFairCodes > 0 && PlainWords.VigenereCodes > 0)
-            {
-                int choice = Random.Range(0, 3);
-                if (choice == 0)
-                    ADFVGXAlgorithm();
-                else if (choice == 1)
-                    PlayFairAlgorithm();
-                else
-                    VigenereAlgorithm();
-            }
-            else if (PlainWords.ADFVGXCodes == 0 && PlainWords.PlayFairCodes > 0 && PlainWords.VigenereCodes > 0)
-            {
-                int choice = Random.Range(0, 2);
-                if (choice == 0)
-                    PlayFairAlgorithm();
-                else
-                    VigenereAlgorithm();
-            }
-            else if (PlainWords.ADFVGXCodes > 0 && PlainWords.PlayFairCodes == 0 && PlainWords.VigenereCodes > 0)
-            {
-                int choice = Random.Range(0, 2);
-                if (choice == 0)
-                    ADFVGXAlgorithm();
-                else
-                    VigenereAlgorithm();
-            }
-            else if (PlainWords.ADFVGXCodes > 0 && PlainWords.PlayFairCodes > 0 && PlainWords.VigenereCodes == 0)
-            {
-                int choice = Random.Range(0, 2);
-                if (choice == 0)
-                    ADFVGXAlgorithm();
-                else
-                    PlayFairAlgorithm();
-            }
-            else if (PlainWords.ADFVGXCodes > 0 && PlainWords.PlayFairCodes == 0 && PlainWords.VigenereCodes == 0)
-                ADFVGXAlgorithm();
-            else if (PlainWords.ADFVGXCodes == 0 && PlainWords.PlayFairCodes > 0 && PlainWords.VigenereCodes == 0)
-                PlayFairAlgorithm();
-            else if (PlainWords.ADFVGXCodes == 0 && PlainWords.PlayFairCodes == 0 && PlainWords.VigenereCodes > 0)
-                VigenereAlgorithm();
-            KeyIndicator.color = KeyColors[keyColorID];
-            CipheredText.text = Level4EncryptedWord;
-        }
-        else
-        {
-            CipheredText.text = "N/A";
-            KeyIndicator.color = KeyColors[5];
-            Solution.enabled = false;
-            SolveReportButton.enabled = false;
-        }
-    }
-
-    public void ADFVGXAlgorithm()
-    {
-        string ADEncKey = keyWords[chosenKey];
-        string allSymbolsTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        string encryptionTable = "";
-        string answerBuffor = Level4Answer.ToUpper();
-
-        while (ADEncKey.Length > 0)
-        {
-            encryptionTable += ADEncKey[0];
-            allSymbolsTable = allSymbolsTable.Replace(ADEncKey[0].ToString(), "");
-            ADEncKey = ADEncKey.Replace(ADEncKey[0].ToString(), "");
-        }
-        encryptionTable += allSymbolsTable;
-
-        int index = 0;
-        int position;
-        while (index < answerBuffor.Length)
-        {
-            position = encryptionTable.IndexOf((char)(answerBuffor[index]));
-            Level4EncryptedWord += ADFVGXTableValues[position / 6];
-            Level4EncryptedWord += ADFVGXTableValues[position % 6];
-            index++;
-        }
-        PlainWords.ADFVGXCodes--;
-    }
-
-    public void PlayFairAlgorithm()
-    {
-        string playEncKey = keyWords[chosenKey];
-        string wholeAlphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
-        string encryptionTable = "";
-        string answerBuffor = Level4Answer.ToUpper();
-
-        while (playEncKey.Length > 0)
-        {
-            encryptionTable += playEncKey[0];
-            wholeAlphabet = wholeAlphabet.Replace(playEncKey[0].ToString(), "");
-            playEncKey = playEncKey.Replace(playEncKey[0].ToString(), "");
-        }
-        encryptionTable += wholeAlphabet;
-
-        if (answerBuffor.Length % 2 == 1)
-            answerBuffor += (char)(answerBuffor[answerBuffor.Length - 1] + 1);
-        answerBuffor = answerBuffor.Replace("J", "I");
-
-        int index = 0;
-        int[] positions = new int[2];
-        int move = 0;
-
-        while (index < answerBuffor.Length)
-        {
-            positions[0] = encryptionTable.IndexOf((char)(answerBuffor[index]));
-            positions[1] = encryptionTable.IndexOf((char)(answerBuffor[index + 1]));
-
-            if (positions[0] / 5 == positions[1] / 5)
-            {
-                Level4EncryptedWord += (positions[0] / 5 == (positions[0] + 1) / 5) ? encryptionTable[positions[0] + 1] : encryptionTable[positions[0] - 4];
-                Level4EncryptedWord += (positions[1] / 5 == (positions[1] + 1) / 5) ? encryptionTable[positions[1] + 1] : encryptionTable[positions[1] - 4];
-            }
-            else if (positions[0] % 5 == positions[1] % 5)
-            {
-                Level4EncryptedWord += (positions[0] + 5 <= 24) ? encryptionTable[positions[0] + 5] : encryptionTable[positions[0] - 20];
-                Level4EncryptedWord += (positions[1] + 5 <= 24) ? encryptionTable[positions[1] + 5] : encryptionTable[positions[1] - 20];
-            }
-            else
-            {
-                if (positions[0] % 5 < positions[1] % 5)
-                {
-                    move = (positions[1] % 5) - (positions[0] % 5);
-                    Level4EncryptedWord += encryptionTable[positions[0] + move];
-                    Level4EncryptedWord += encryptionTable[positions[1] - move];
-                }
-                else
-                {
-                    move = (positions[0] % 5) - (positions[1] % 5);
-                    Level4EncryptedWord += encryptionTable[positions[0] - move];
-                    Level4EncryptedWord += encryptionTable[positions[1] + move];
-                }
-            }
-            index += 2;
-        }
-        PlainWords.PlayFairCodes--;
-    }
-
-    public void VigenereAlgorithm()
-    {
-        int index = 0;
-        int keyIndex = 0;
-        char letter;
-
-        while (index < Level4Answer.Length)
-        {
-            letter = (char)((Level4Answer[index] - 32) + (keyWords[chosenKey][keyIndex] - 65));
-            if (letter > 90)
-                letter = (char)(letter - 26);
-            Level4EncryptedWord += letter;
-            keyIndex = (keyIndex + 1) >= keyWords[chosenKey].Length ? 0 : (keyIndex + 1);
-            index++;
-        }
-        PlainWords.VigenereCodes--;
-    }
-
+    
     public void SendSolutionReport()
     {
-        if (Solution.text.ToLower() == Level4Answer)
+        if (AnswerField.text.ToLower() == "TEST")
         {
             hideTimer = 2;
             StartCoroutine(ShowResult());
             ResultMessage.color = Color.green;
             ResultMessage.text = "Correct";
-            AccessEnigmaScript.decryptedMessagesLevelFour++;
-            ADFVGX_PlayFair_Vigenere_EncryptFunction();
-            if (AccessEnigmaScript.decryptedMessagesLevelFour <= goal)
-            {
-                ProgressBar.value = (float)AccessEnigmaScript.decryptedMessagesLevelFour / (float)goal;
-                ProgressValue.text = Mathf.RoundToInt(ProgressBar.value * 100).ToString() + "%";
-
-                if (AccessEnigmaScript.decryptedMessagesLevelFour == goal)
-                {
-                    AccessEnigmaScript.Level4Completed = true;
-                    AccountPassword.gameObject.SetActive(true);
-                }
-
-                ///THIS CONTENT IS MOVED TO FINAL VERSION
-                //if (decryptedMessagesLevelThree == goal)
-                //{
-                //    PlainWords.MorseCodes = 7;
-                //    PlainWords.XORCodes = 7;
-                //    Morse_XOR_EncryptFunction();
-                //}
-            }
+            
         }
         else
         {
@@ -516,7 +324,7 @@ public class EnigmaLevelScripts : MonoBehaviour
             }
         }
         ResultMessage.gameObject.SetActive(true);
-        Solution.text = "";
+        AnswerField.text = "";
     }
 
     private IEnumerator ShowResult()
@@ -530,25 +338,4 @@ public class EnigmaLevelScripts : MonoBehaviour
         ResultMessage.gameObject.SetActive(false);
         yield break;
     }
-
-    private IEnumerator ShowError()
-    {
-        while (hideTimer > 0)
-        {
-            yield return new WaitForSeconds(1);
-            hideTimer--;
-        }
-
-        if (ActiveTool == EncryptTools.Vigenere)
-            VigenereError.gameObject.SetActive(false);
-        else if (ActiveTool == EncryptTools.PlayFair)
-            PlayFairError.gameObject.SetActive(false);
-        else if (ActiveTool == EncryptTools.ADFVGX)
-            ADFVGXError.gameObject.SetActive(false);
-
-        yield break;
-    }
-
-    public void LogOutFunc() =>
-        SceneManager.LoadSceneAsync("Game_AllUsers");*/
 }
